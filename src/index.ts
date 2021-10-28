dd7606cec66e0ffcd7a62226cc76f78624d92da3
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2021-10-28 10:16:48 +0200

    start

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+const Web3 = require("web3");
+
+export class IdrissCrypto {
+    private web3;
+
+    constructor(ethEndpoint: string = "https://bsc-dataseed.binance.org/") {
+        this.web3 = new Web3(new Web3.providers.HttpProvider(ethEndpoint));
+
+    }
+
+    public async resolve(input: string) {
+        const contract = this.generateContract();
+        let result = await contract.methods.Idriss(input).call()
+        debugger;
+        console.log({result});
+    }
+
+    private generateContract() {
+        return new this.web3.eth.Contract([{
+            "anonymous": false,
+            "inputs": [{"indexed": false, "internalType": "uint256", "name": "value", "type": "uint256"}],
+            "name": "Increment",
+            "type": "event"
+        }, {
+            "inputs": [{"internalType": "string", "name": "", "type": "string"}],
+            "name": "Idriss",
+            "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [{"internalType": "string", "name": "hash", "type": "string"}, {
+                "internalType": "string",
+                "name": "id",
+                "type": "string"
+            }], "name": "addIdriss", "outputs": [], "stateMutability": "payable", "type": "function"
+        }, {
+            "inputs": [],
+            "name": "countAdding",
+            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [],
+            "name": "countDeleting",
+            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [],
+            "name": "creationTime",
+            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [{"internalType": "string", "name": "hash", "type": "string"}],
+            "name": "deleteIdriss",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+        }, {
+            "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+            "name": "mapKeys",
+            "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [],
+            "name": "minimumAdding",
+            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [],
+            "name": "noPriceTime",
+            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [],
+            "name": "price",
+            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+            "stateMutability": "view",
+            "type": "function"
+        }, {
+            "inputs": [{"internalType": "uint256", "name": "newPrice", "type": "uint256"}],
+            "name": "setPrice",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }, {
+            "inputs": [],
+            "name": "withdrawl",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }], '0xab39e7c21b4a1d0f56a59699f0196d59efd739a5');
+    }
+}
---
+++ tests/e2e/index.js
@@ -0,0 +1,4 @@
+const {IdrissCrypto} = require("../../lib");
+
+const obj = new IdrissCrypto()
+const result = obj.resolve("mateusz.slon@gmail.com")
