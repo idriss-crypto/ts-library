1ea2ead71772215ddbcd2a62f6a23c686bfd5f3a
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2021-11-02 20:42:40 +0100

    refactor: cleaning code

--- src/webApi.ts
+++ src/webApi.ts
@@ -1,11 +1,13 @@
 import fetch from "node-fetch";
+
 export class WebApi {
-    async encrypt(inputCombination: string):Promise<EncryptResponse> {
+    async encrypt(inputCombination: string): Promise<EncryptResponse> {
         const response = await fetch(`https://www.idriss-crypto.com//v1/encrypt?InputCombination=${encodeURIComponent(inputCombination)}`)
-        if(response.status!=200) throw new Error("Idriss api responded with code "+response.status+" "+response.statusText)
+        if (response.status != 200) throw new Error("Idriss api responded with code " + response.status + " " + response.statusText)
         return await (response.json());
     }
 }
+
 interface EncryptResponse {
-    result:{[index:string]:string}
+    result: { [index: string]: string }
 }
