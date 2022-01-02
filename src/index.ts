b75fb542b882f98ab80bda14f44df032bac6e3e4
idrisssystem <87615244+idrisssystem@users.noreply.github.com>
2022-01-02 14:05:24 -0500

    Update index.ts
    Polygon smart contract support

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ export class IdrissCrypto {
     private webApi;
     private contract;
 
-    constructor(bscEndpoint: string = "https://bsc-dataseed.binance.org/") {
-        this.web3 = new Web3(new Web3.providers.HttpProvider(bscEndpoint));
+    constructor(polygonEndpoint: string = "https://polygon-rpc.com/") {
+        this.web3 = new Web3(new Web3.providers.HttpProvider(polygonEndpoint));
         this.webApi = new WebApi()
         this.contract = this.generateContract();
     }
@@ -25,89 +25,287 @@ export class IdrissCrypto {
     }
 
     private generateContract() {
-        return new this.web3.eth.Contract([{
-            "anonymous": false,
-            "inputs": [{"indexed": false, "internalType": "uint256", "name": "value", "type": "uint256"}],
-            "name": "Increment",
-            "type": "event"
-        }, {
-            "inputs": [{"internalType": "string", "name": "", "type": "string"}],
-            "name": "Idriss",
-            "outputs": [{"internalType": "string", "name": "", "type": "string"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [{"internalType": "string", "name": "hash", "type": "string"}, {
-                "internalType": "string",
-                "name": "id",
-                "type": "string"
-            }], "name": "addIdriss", "outputs": [], "stateMutability": "payable", "type": "function"
-        }, {
-            "inputs": [],
-            "name": "countAdding",
-            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [],
-            "name": "countDeleting",
-            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [],
-            "name": "creationTime",
-            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [{"internalType": "string", "name": "hash", "type": "string"}],
-            "name": "deleteIdriss",
-            "outputs": [],
-            "stateMutability": "payable",
-            "type": "function"
-        }, {
-            "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-            "name": "mapKeys",
-            "outputs": [{"internalType": "string", "name": "", "type": "string"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [],
-            "name": "minimumAdding",
-            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [],
-            "name": "noPriceTime",
-            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [],
-            "name": "owner",
-            "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [],
-            "name": "price",
-            "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-            "stateMutability": "view",
-            "type": "function"
-        }, {
-            "inputs": [{"internalType": "uint256", "name": "newPrice", "type": "uint256"}],
-            "name": "setPrice",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }, {
-            "inputs": [],
-            "name": "withdrawl",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }], '0xab39e7c21b4a1d0f56a59699f0196d59efd739a5');
+        return new this.web3.eth.Contract(
+            
+        [
+            {
+                "inputs": [
+                    {
+                        "internalType": "string",
+                        "name": "hash",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "id",
+                        "type": "string"
+                    }
+                ],
+                "name": "addIdriss",
+                "outputs": [],
+                "stateMutability": "payable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "string",
+                        "name": "hash",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "id",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "token",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "addIdrissToken",
+                "outputs": [],
+                "stateMutability": "payable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "string",
+                        "name": "hash",
+                        "type": "string"
+                    }
+                ],
+                "name": "deleteIdriss",
+                "outputs": [],
+                "stateMutability": "payable",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "stateMutability": "nonpayable",
+                "type": "constructor"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "uint256",
+                        "name": "value",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "Increment",
+                "type": "event"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "newPrice",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "setPrice",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "withdrawl",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "tokenContract",
+                        "type": "address"
+                    }
+                ],
+                "name": "withdrawTokens",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "countAdding",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "countDeleting",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "creationTime",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "name": "Idriss",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "mapKeys",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "minimumAdding",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "noPriceIncrease",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "owner",
+                "outputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "name": "payDates",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "name": "Payments",
+                "outputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "price",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            }
+        ]
+            , '0x98813B43d03B69C2408105326aE5fD66e0F8D374');
     }
 }
