84d682cb4d3f28972b39fea18c698058d3bde68e
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2022-01-20 19:00:33 +0100

    removing unnessesary node dependency

--- src/webApi.ts
+++ src/webApi.ts
@@ -1,17 +1,16 @@
 import fetch from "node-fetch";
 import {ResolveOptions} from "./resolveOptions";
-import {URL} from "url";
-
 
 export class WebApi {
     async encrypt(inputCombination: string, options: ResolveOptions = {}): Promise<EncryptResponse> {
-        const url = new URL("https://www.idriss-crypto.com//v1/encrypt");
-        url.searchParams.append("InputCombination", inputCombination);
+        const url = "https://www.idriss-crypto.com//v1/encrypt";
+        const searchParams = [];
+        searchParams.push(["InputCombination", inputCombination]);
         if (options.coin != null)
-            url.searchParams.append("coin", options.coin);
+            searchParams.push(["coin", options.coin]);
         if (options.network != null)
-            url.searchParams.append("network", options.network);
-        const response = await fetch(url)
+            searchParams.push(["network", options.network]);
+        const response = await fetch(url + '?' + searchParams.map(x => encodeURIComponent(x[0]) + '=' + encodeURIComponent(x[1])).join('&'))
         if (response.status != 200) throw new Error("Idriss api responded with code " + response.status + " " + response.statusText + "\r\n" + await response.text())
         return await (response.json());
     }
