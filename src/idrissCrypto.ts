8a546754a4189cf14194ebef524441d014d9a7e4
deliriusz <kalinowskirafal.fm@gmail.com>
2022-07-15 12:57:41 +0200

    adding connection options to allow for passing own web3 provider

--- src/idrissCrypto.ts
+++ src/idrissCrypto.ts
@@ -1,16 +1,24 @@
 import {BaseIdrissCrypto} from "./baseIdrissCrypto";
+import { ConnectionOptions } from "./types/connectionOptions";
 
 
 var crypto = require('crypto');
 
 export class IdrissCrypto extends BaseIdrissCrypto {
-    constructor(polygonEndpoint: string = "https://polygon-rpc.com/") {
+    constructor(polygonEndpoint: string = "https://polygon-rpc.com/", connectionOptions: ConnectionOptions = {}) {
         const Web3Promise = import("web3");
-        super(Web3Promise.then(x=>x.default).then(Web3=>new Web3(new Web3.providers.HttpProvider(polygonEndpoint))));
-    }
+        super(Web3Promise
+            .then(x=>x.default)
+            .then(Web3=>new Web3(
+                connectionOptions.web3Provider ?? new Web3.providers.HttpProvider(polygonEndpoint)
+        )));
+        }
 
     protected async digestMessage(message: string):Promise<string> {
         return crypto.createHash('sha256').update(message).digest('hex');
     }
 
+    protected async getConnectedAccount(): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
 }
---
+++ src/types/connectionOptions.ts
@@ -0,0 +1,13 @@
+import {provider} from 'web3-core'
+
+/**
+ * Those are optional configuration items that can be passed to IDrissCrypto
+ */
+export type ConnectionOptions = {
+   //overrides default HttpWeb3Provider, which only supports calling blockchain, and not sending transactions to modify it
+   web3Provider?: provider,
+   // overriding contract addresses is added as a helper for testing purposes. It should not be changed in other cases
+   sendToAnyoneContractAddress?: string,
+   idrissRegistryContractAddress?: string,
+   reverseIDrissMappingContractAddress?: string,
+}
