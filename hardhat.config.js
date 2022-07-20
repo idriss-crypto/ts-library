23e2f17e63b6a2bda4010c43cfbe7ddc3fe6e173
deliriusz <kalinowskirafal.fm@gmail.com>
2022-07-20 22:30:21 +0200

    sharing accounts settings across all test config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,17 @@
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-ethers");
 
+const testAccounts = {
+  mnemonic: "test test test test test test test test test test test junk",
+      path: "m/44'/60'/0'/0",
+      initialIndex: 0,
+      count: 20,
+      passphrase: "",
+}
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.7",
   paths: {
     artifacts: 'tests/artifacts',
     sources: 'tests/contracts',
@@ -16,21 +24,17 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
+      accounts: testAccounts,
     },
     ganache: {
       chainId: 1337, //event though config says it's 5777
       url: "http://127.0.0.1:7545",
+      accounts: testAccounts,
     },
     hardhat_node: {
       chainId: 1337,
       url: "http://127.0.0.1:8545",
-      accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: "",
-      },
+      accounts: testAccounts,
     }
   }
 };
