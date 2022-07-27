0cf6dbad8fb4b14b09a21b5b4c51379e2de7cea6
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-27 19:31:23 +0200

    bundle fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = {
-    mode: "production", // "production" | "development" | "none"
-    entry: {
-        "global": "./lib/esnext/bundleGlobal.js",
-        "module": "./lib/esnext/browser.js",
-    },
-    output: {
-        library: {type: 'module'},
-        path: path.resolve(__dirname, 'lib/bundle'),
-        filename:'[name].js'
-    },
-    experiments:{outputModule:true},
+module.exports = env => {
+    if (env.m == 1) {
+        return {
+            mode: "production", // "production" | "development" | "none"
+            entry: {
+                "global": "./lib/esnext/bundleGlobal.js",
+            },
+            output: {
+                path: path.resolve(__dirname, 'lib/bundle'),
+                filename: '[name].js'
+            },
+        }
+    } else {
+        return {
+            mode: "production", // "production" | "development" | "none"
+            entry: {
+                "module": "./lib/esnext/browser.js",
+            },
+            output: {
+                library: {type: 'module'},
+                path: path.resolve(__dirname, 'lib/bundle'),
+                filename: '[name].js'
+            },
+            experiments: {outputModule: true},
+        }
+    }
 }
