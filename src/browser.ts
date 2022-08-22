23921c069a7d839289012db39f853ff3d114e7db
deliriusz <kalinowskirafal.fm@gmail.com>
2022-08-22 12:00:32 +0200

    browser cleanup

--- src/browser.ts
+++ src/browser.ts
@@ -7,18 +7,14 @@ export class IdrissCrypto extends BaseIdrissCrypto {
         super(Web3Promise
             .then(x=>x.default)
             .then(Web3=>new Web3(
-                // first check if there is a provider passed in the request,
-                // then check if ethereum is injected to the browser
-                // otherwise use read-only HttpProvider
                 connectionOptions.web3Provider
                 ?? new Web3.providers.HttpProvider(polygonEndpoint)
-                ?? this.getInjectedEthereum()
         )), connectionOptions);
     }
 
     protected async digestMessage(message:string) {
         const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
-        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);  // hash the message
         const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
         const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
         return hashHex;
