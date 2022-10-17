cc085fb798375b45a009948b67b6199b9784ea37
deliriusz <deliriusz.eth@gmail.com>
2022-10-17 10:33:49 +0200

    adding contract sizer utility & enabling hardhat optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-ethers");
+require('hardhat-contract-sizer');
 
 const testAccounts = {
   mnemonic: "test test test test test test test test test test test junk",
@@ -11,7 +12,15 @@ const testAccounts = {
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.7",
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 300
+      }
+    }
+  },
   paths: {
     artifacts: 'tests/artifacts',
     sources: 'tests/contracts/src/contracts',
@@ -25,16 +34,19 @@ module.exports = {
     hardhat: {
       chainId: 1337,
       accounts: testAccounts,
+      allowUnlimitedContractSize: true
     },
     ganache: {
       chainId: 1337, //event though config says it's 5777
       url: "http://127.0.0.1:7545",
       accounts: testAccounts,
+      allowUnlimitedContractSize: true
     },
     hardhat_node: {
       chainId: 1337,
       url: "http://127.0.0.1:8545",
       accounts: testAccounts,
+      allowUnlimitedContractSize: true
     }
   }
 };
